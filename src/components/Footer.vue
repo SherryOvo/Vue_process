1ab3c2edfdac36c432a70eeb39387b8724657d0c
sd_my196 <1411743410@qq.com>
2022-09-19 22:04:11 +0800

    修改Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
 	<ul class="footer">
-		<li onclick="location.href='index.html'">
+		<li @click="toIndex">
 			<i class="fa fa-home"></i>
 			<p>首页</p>
 		</li>
@@ -8,7 +8,7 @@
 			<i class="fa fa-compass"></i>
 			<p>发现</p>
 		</li>
-		<li onclick="location.href='orderList.html'">
+		<li @click="toOrderList">
 			<i class="fa fa-file-text-o"></i>
 			<p>订单</p>
 		</li>
@@ -20,32 +20,43 @@
 </template>
 
 <script>
+	export default {
+		name: 'Footer',
+		methods: {
+			toIndex() {
+				this.$router.push({
+					path: '/index'
+				});
+			},
+			toOrderList() {
+				this.$router.push({
+					path: '/orderList'
+				});
+			}
+		}
+	}
 </script>
 
 <style>
 	.wrapper .footer {
 		width: 100%;
 		height: 14vw;
-		border-top: solid 1px #ddd;
+		border-top: solid 1px #DDD;
 		background-color: #fff;
-
 		position: fixed;
-		/* 固定定位 */
-		/* 就脱离文档流,可以设坐标 */
 		left: 0;
 		bottom: 0;
-
 		display: flex;
 		justify-content: space-around;
 		align-items: center;
 	}
 
 	.wrapper .footer li {
+		/*li本身的尺寸完全由内容撑起*/
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
 		align-items: center;
-
 		color: #999;
 		user-select: none;
 		cursor: pointer;
